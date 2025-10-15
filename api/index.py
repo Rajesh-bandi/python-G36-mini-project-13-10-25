f8ce03148ee9be6dd1f6a8334e1d0453db3a1360
BANDI RAJESH <bandirajesh209@gmail.com>
2025-10-15 19:34:15 +0530

    Delete api directory

--- api/index.py
+++
@@ -1,6 +0,0 @@
-from task_tracker.main import app
-
-# Vercel's Python runtime expects an ASGI 'app' object at module-level.
-# This file is the serverless entrypoint used by Vercel when deploying from the GUI.
-
-__all__ = ("app",)
